6083ca8566fdbbe9d4514aad7ce475ecb73bb6d6
lmnjcx <81728420+ssssshel@users.noreply.github.com>
2022-04-30 10:55:59 -0500

    a

--- pages/admin/[id]/panel/products/index.jsx
+++ pages/admin/[id]/panel/products/index.jsx
@@ -1,13 +1,121 @@
+import { useRouter } from "next/router";
+
 import HeadLayout from "../../../../../components/Head";
 import Navbar from "../../../../../components/Navbar";
 
 const ProductsPanel = () => {
-    return ( 
-        <div>
-            <HeadLayout section={'Panel de productos'}/>
-            <Navbar />
+  const router = useRouter();
+
+  return (
+    <div>
+      <HeadLayout section={"Panel de productos"} />
+      <Navbar />
+      <div className="flex flex-col w-full min-h-screen gap-6 px-10 py-20 bg-ivory">
+        <div className="flex flex-col justify-center w-full h-20 px-6 shadow-xl rounded-2xl bg-green">
+          <p className="text-3xl font-Pacifico text-charleston">
+            Panel de productos
+          </p>
         </div>
-     );
-}
- 
+        <div className="grid grid-cols-10 grid-rows-1 gap-4">
+          {/* OPCIONES INVENTARIO */}
+          <div className="flex flex-col justify-center col-start-1 col-end-7 gap-5 px-6 py-4 shadow-xl bg-cornsilk rounded-xl">
+            <p className="text-xl font-Pacifico text-charleston">Inventario</p>
+            <div className="p-3 rounded-lg cursor-pointer hover:bg-green bg-teal">
+              <p
+                onClick={() =>
+                  router.push("/admin/ADMINID/panel/products/abrigos")
+                }
+                className="text-ivory "
+              >
+                Abrigos
+              </p>
+            </div>
+            <div className="p-3 rounded-lg cursor-pointer hover:bg-green bg-teal">
+              <p
+                onClick={() =>
+                  router.push("/admin/ADMINID/panel/products/casacas")
+                }
+                className="text-ivory "
+              >
+                Casacas
+              </p>
+            </div>
+            <div className="p-3 rounded-lg cursor-pointer hover:bg-green bg-teal">
+              <p
+                onClick={() =>
+                  router.push("/admin/ADMINID/panel/products/chompas")
+                }
+                className="text-ivory "
+              >
+                Chompas
+              </p>
+            </div>
+            <div className="p-3 rounded-lg cursor-pointer hover:bg-green bg-teal">
+              <p
+                onClick={() =>
+                  router.push("/admin/ADMINID/panel/products/joggers")
+                }
+                className="text-ivory "
+              >
+                Joggers
+              </p>
+            </div>
+            <div className="p-3 rounded-lg cursor-pointer hover:bg-green bg-teal">
+              <p
+                onClick={() =>
+                  router.push("/admin/ADMINID/panel/products/jeans")
+                }
+                className="text-ivory "
+              >
+                Jeans
+              </p>
+            </div>
+            <div className="p-3 rounded-lg cursor-pointer hover:bg-green bg-teal">
+              <p
+                onClick={() =>
+                  router.push("/admin/ADMINID/panel/products/faldas")
+                }
+                className="text-ivory "
+              >
+                Faldas
+              </p>
+            </div>
+            <div className="p-3 rounded-lg cursor-pointer hover:bg-green bg-teal">
+              <p
+                onClick={() =>
+                  router.push("/admin/ADMINID/panel/products/blusas")
+                }
+                className="text-ivory "
+              >
+                Blusas
+              </p>
+            </div>
+            <div className="p-3 rounded-lg cursor-pointer hover:bg-green bg-teal">
+              <p
+                onClick={() =>
+                  router.push("/admin/ADMINID/panel/products/polos")
+                }
+                className="text-ivory "
+              >
+                Polos
+              </p>
+            </div>
+            <div className="p-3 rounded-lg cursor-pointer hover:bg-green bg-teal">
+              <p
+                onClick={() =>
+                  router.push("/admin/ADMINID/panel/products/shorts")
+                }
+                className="text-ivory "
+              >
+                Shorts
+              </p>
+            </div>
+          </div>
+          <div>Agregar nuevo</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default ProductsPanel;
--- pages/admin/[id]/panel/users/index.jsx
+++ pages/admin/[id]/panel/users/index.jsx
@@ -2,26 +2,30 @@ import HeadLayout from "../../../../../components/Head";
 import Navbar from "../../../../../components/Navbar";
 
 const UsersPanel = () => {
-    return ( 
-        <div>
-            <HeadLayout section={"Panel de usuarios"}/>
-            <Navbar />
-            <div className="flex flex-col w-full min-h-screen gap-6 px-10 py-20 bg-ivory">
-                <div className="flex flex-col justify-center w-full h-20 px-6 shadow-xl rounded-2xl bg-teal">
-                    <p className="text-3xl font-Pacifico text-charleston">Panel de usuarios</p>
-                </div>
-                <div className="flex flex-col gap-4">
-                <div className="flex items-center justify-between px-6 py-4 shadow-xl bg-cornsilk rounded-xl">
-                        <p className="text-lg font-Comfortaa text-charleston">Angel Arteaga</p>
-                        <div className="text-sm">
-                        <p className="cursor-pointer">Usuario desde:</p>
-                        <p className="cursor-pointer">Ver detalle</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <HeadLayout section={"Panel de usuarios"} />
+      <Navbar />
+      <div className="flex flex-col w-full min-h-screen gap-6 px-10 py-20 bg-ivory">
+        <div className="flex flex-col justify-center w-full h-20 px-6 shadow-xl rounded-2xl bg-green">
+          <p className="text-3xl font-Pacifico text-charleston">
+            Panel de usuarios
+          </p>
         </div>
-     );
-}
- 
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center justify-between px-6 py-4 shadow-xl bg-cornsilk rounded-xl">
+            <p className="text-lg font-Comfortaa text-charleston">
+              Angel Arteaga
+            </p>
+            <div className="text-sm">
+              <p className="cursor-pointer">Usuario desde:</p>
+              <p className="cursor-pointer">Ver detalle</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default UsersPanel;
