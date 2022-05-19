6a680ec01ae19b660777434e1c7951f5b3acff94
lmnjcx <81728420+ssssshel@users.noreply.github.com>
2022-05-19 18:46:54 -0500

    fb env

--- firebase/credentials.js
+++ firebase/credentials.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDFXLGR3faylWjEvRgSQ8elIHubEsnO4k4",
+  apiKey: process.env.APIKEY,
   authDomain: "glorious-vs.firebaseapp.com",
   projectId: "glorious-vs",
   storageBucket: "glorious-vs.appspot.com",
-  messagingSenderId: "947463447717",
-  appId: "1:947463447717:web:928ff4284452d20c17194c",
-  measurementId: "G-S1B38DEEQ9"
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: proccess.env.APPID,
+  measurementId: process.env.MEASUREMENTID
 };
 
 // Initialize Firebase
