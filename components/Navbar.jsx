31e6d1f7a9704e91e884629b25c5f9926c477ce0
lmnjcx <81728420+ssssshel@users.noreply.github.com>
2022-05-19 21:18:28 -0500

    test modules added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
-import {useRouter} from "next/router"
-// import { signIn, signOut, useSession } from "next-auth/react"
+import { useRouter } from "next/router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
 
-const Navbar = ({hasPrivileges, clientID}) => {
-
-  const router = useRouter()
+const Navbar = ({ hasPrivileges, clientID }) => {
+  const router = useRouter();
 
   // const { data: session, status } = useSession()
 
-  {/* AUTHENTICATED STATE */ }
+  {
+    /* AUTHENTICATED STATE */
+  }
   // if (!hasPrivileges ) {
   //   return (
   //     <header className="fixed top-0 z-50 flex flex-row items-center justify-between w-full h-20 px-10 backdrop-blur-lg navbar bg-ivory/80 ">
@@ -33,11 +33,13 @@ const Navbar = ({hasPrivileges, clientID}) => {
   //           </a>
   //         </Link>
   //         <a onClick={() => signOut({callbackUrl: "/"})} className="hover:font-bold ">Cerrar sesión</a>
-          
+
   //       </ul>
   //     </header>
   //   )
-    {/* AUTHENTICATED ADMIN */ }
+  {
+    /* AUTHENTICATED ADMIN */
+  }
   // } else if (hasPrivileges === true && status === "authenticated") {
   //   return (
   //     <header className="fixed top-0 z-10 flex flex-row items-center justify-between w-full h-20 px-10 backdrop-blur-lg navbar bg-ivory/80 ">
@@ -52,40 +54,36 @@ const Navbar = ({hasPrivileges, clientID}) => {
   //           <a className="hover:font-bold ">Panel de administración</a>
   //         </Link>
   //         <a onClick={() => signOut({callbackUrl: "/"})} className="hover:font-bold ">Cerrar sesión</a>
-          
 
   //       </ul>
   //     </header>
   //   )
-  {/* NAVBAR VISITOR */ }
+  {
+    /* NAVBAR VISITOR */
+  }
 
-  
-    return (
-      <header className="fixed top-0 z-10 flex flex-row items-center justify-between w-full h-20 px-10 backdrop-blur-lg navbar bg-ivory/80 ">
+  return (
+    <header className="fixed top-0 z-10 flex flex-row items-center justify-between w-full h-20 px-10 backdrop-blur-lg navbar bg-ivory/80 ">
+      <Link href="/">
+        <a className="text-3xl font-Pacifico text-charleston">Glorious</a>
+      </Link>
+      <ul className="flex flex-row gap-12 font-Comfortaa">
         <Link href="/">
-          <a className="text-3xl font-Pacifico text-charleston">Glorious</a>
+          <a className="hover:font-bold ">Inicio</a>
         </Link>
-        <ul className="flex flex-row gap-12 font-Comfortaa">
-          <Link href="/">
-            <a className="hover:font-bold ">Inicio</a>
-          </Link>
-          <Link href="/catalogo">
-            <a className="hover:font-bold ">Productos</a>
-          </Link>
-          <Link href="/access/login">
+        <Link href="/catalogo">
+          <a className="hover:font-bold ">Productos</a>
+        </Link>
+        <Link href="/access/login">
           <a className="hover:font-bold">Iniciar sesión</a>
-          
-          </Link>
+        </Link>
 
-          <Link href="/access/register">
-            <a className="hover:font-bold ">Registro</a>
-          </Link>
-        </ul>
-      </header>
-    );
-  
-
-
+        <Link href="/access/register">
+          <a className="hover:font-bold ">Registro</a>
+        </Link>
+      </ul>
+    </header>
+  );
 };
 
 export default Navbar;
